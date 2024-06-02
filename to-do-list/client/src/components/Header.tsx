d64288cc5bcc2ef843fb4fb84770b1957381d4ec
Zakaria Rabhi <zrabhi@e2r13p5.1337.ma>
2024-06-02 15:20:21 +0100

    Readme file updated

--- to-do-list/client/src/components/Header.tsx
+++ to-do-list/client/src/components/Header.tsx
@@ -1,35 +1,32 @@
-import { HeaderProp } from "@/utils/interfaces"
+import { HeaderProp } from "@/utils/interfaces";
 import Image from "next/image";
 import Picture from "@/utils/Image.png";
-import Link from "next/link"
-import { FC } from "react"
-export const Header : FC<HeaderProp> = ({name}) =>{
+import Link from "next/link";
+import { FC } from "react";
+export const Header: FC<HeaderProp> = ({ name }) => {
   const handleLogout = () => {
     // Remove the token and user from the local storage
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-  
-  }
-    return (<div className="flex justify-between items-center p-4 sm:m-24 m-10">
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+  return (
+    <div className="flex justify-between items-center p-4 sm:m-24 m-10">
       <div className="flex  flex-row">
-      <img
-            src={Picture.src}
-            className="w-16 h-16"
-            alt="image"
-            />
-          <h1 className="sm:text-4xl text-2xl text-Purple font-bold font-itim ">
-            TO-DO-NOW
-          </h1>
+        <img src={Picture.src} className="w-16 h-16" alt="image" />
+        <h1 className="sm:text-4xl text-2xl text-Purple font-bold font-itim ">
+          TO-DO-NOW
+        </h1>
       </div>
-    <Link href={` ${name ==="Sign Up" ? "/" : "/login"}`}>
-      <button className="sm:text-2xl sm:h-14 sm:w-36 w-28 h-8 text-[#ffff] text-white font-bold font-inter rounded-lg bg-Purple"
-      onClick={()=>{
-        if (name === "logout")
-            handleLogout();
-          }} >
-        {name}
-      </button>
-    </Link>
-  </div>
-  )
-}
+      <Link href={` ${name === "Sign Up" ? "/" : "/login"}`}>
+        <button
+          className="sm:text-2xl sm:h-14 sm:w-36 w-28 h-8 text-[#ffff] text-white font-bold font-inter rounded-lg bg-Purple"
+          onClick={() => {
+            if (name === "logout") handleLogout();
+          }}
+        >
+          {name}
+        </button>
+      </Link>
+    </div>
+  );
+};
