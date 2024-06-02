63ab27a7b48e4f285a14fbdd3ff352588146ac82
Zakaria Rabhi <zrabhi@e2r13p5.1337.ma>
2024-06-02 15:17:32 +0100

    minor changes in header component

--- to-do-list/client/src/components/Header.tsx
+++ to-do-list/client/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { HeaderProp } from "@/utils/interfaces"
+import Image from "next/image";
+import Picture from "@/utils/Image.png";
 import Link from "next/link"
 import { FC } from "react"
 export const Header : FC<HeaderProp> = ({name}) =>{
@@ -8,10 +10,17 @@ export const Header : FC<HeaderProp> = ({name}) =>{
     localStorage.removeItem('user');
   
   }
-    return (<div className="flex justify-between items-center p-4 sm:mr-24">
-    <h1 className="sm:text-4xl text-2xl text-Purple font-bold font-itim sm:m-24 m-10">
-      TO-DO-NOW
-    </h1>
+    return (<div className="flex justify-between items-center p-4 sm:m-24 m-10">
+      <div className="flex  flex-row">
+      <img
+            src={Picture.src}
+            className="w-16 h-16"
+            alt="image"
+            />
+          <h1 className="sm:text-4xl text-2xl text-Purple font-bold font-itim ">
+            TO-DO-NOW
+          </h1>
+      </div>
     <Link href={` ${name ==="Sign Up" ? "/" : "/login"}`}>
       <button className="sm:text-2xl sm:h-14 sm:w-36 w-28 h-8 text-[#ffff] text-white font-bold font-inter rounded-lg bg-Purple"
       onClick={()=>{
